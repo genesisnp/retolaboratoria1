a4fe980ace763f4d7112ceb6749b1dfacc213b90
genesis nole <genesis.np28@gmail.com>
2018-05-04 00:39:33 -0500

    ultimo

--- app.js
+++ app.js
@@ -1,33 +1,124 @@
 var name = prompt('¿Cuál es tu Nombre');
 var menu_bienvenido = document.getElementById('menu_bienvenidoHtml');
-menu_bienvenido . innerHTML=' Bienvenido : ' + name.toUpperCase();
+
+menu_bienvenido.innerHTML=' Bienvenido : ' + name.toUpperCase();
 
 if(confirm(' Hola! ' + name.toUpperCase()  +'\n ¿Te Animas a Jugar? ')){
-  alert ( ' Muy bien ' + name.toUpperCase() + ' entonces comencemos! \n POR FAVOR SELECCIONA QUE CATEGORIA QUIERES JUGAR: \n '+ '1.DEPORTES \n 2.CVULTURA GENERAL \n 3.TECNOLOGIA \n 4.PELICULAS');
-  } else {
+  alert( ' Muy bien ' + name.toUpperCase() + ' Entonces Comencemos!');
+  preguntas();
+} else {
       alert (' Que lastima, visitanos pronto');
-  }
-  var pregunta1 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
-    var respuesta1 = "si"
-    //JavaScript fue desarrollado originalmente por Brendan Eich.
-    if (pregunta1 == respuesta1) {
-          alert ('¡Genial! Respuesta correcta');
-  }else{
-    alert ('Respuesta incorrecta')};
+}
 
-    var pregunta2 = prompt('¿Es HTML un lenguaje de programación?');
-    var respuesta2 = "no"
-    //HTML es un lenguaje de marcado de hipertexto, que da estructura a un documento mediante etiquetas.
-    if (pregunta2 == respuesta2) {
-      alert ('¡Genial! Respuesta correcta')
-  }else {
-    alert ('Respuesta incorrecta');
-
-  }
-    var pregunta3 = prompt('¿Para declarar estilos en CSS necesitas usar Propiedades y valores?');
-    var respuesta3 = "si"
-    //El par formado por una propiedad y un valor se denomina declaración CSS.
-    if (pregunta3 == respuesta3) { alert ('¡Genial! Respuesta correcta')
-    }else {
-      alert ('Respuesta incorrecta')
-    };
+function preguntas(){
+	var categoria =prompt('POR FAVOR SELECCIONA QUE CATEGORIA QUIERES JUGAR: \n '+ '1.DEPORTES \n 2.CULTURA GENERAL \n 3.TECNOLOGIA \n 4.PELICULAS');
+	switch (categoria){
+		case('1'):
+			var pregunta1 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var pregunta2 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var pregunta3 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var respuesta1 = "si",
+				respuesta2 = "no",
+				respuesta3 = "si",
+				puntaje1	= 0 ;
+			if (pregunta1 == respuesta1) {
+	    		puntaje1++;
+			}
+			if(pregunta2 == respuesta2){
+	    		puntaje1++;
+			}
+			if(pregunta3 == respuesta3){
+	    		puntaje1++;
+			}
+			console.log(puntaje1);
+			if(confirm("Desea seguir jugando?")){
+				preguntas();
+			}else{
+				alert("adios");
+				document.getElementById('btn_play').style.display="block";
+				document.getElementsByClassName('categoria_uno_r')[0].innerHTML = "Puntos: "+puntaje1;
+			}
+			break;
+		case('2'):
+			var pregunta1 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var pregunta2 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var pregunta3 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var respuesta1 = "si",
+				respuesta2 = "no",
+				respuesta3 = "si",
+				puntaje2	   = 0 ;
+			if (pregunta1 == respuesta1) {
+	    		puntaje2++;
+			}
+			if(pregunta2 == respuesta2){
+	    		puntaje2++;
+			}
+			if(pregunta3 == respuesta3){
+	    		puntaje2++;
+			}
+			console.log(puntaje2);
+			if(confirm("Desea seguir jugando?")){
+				preguntas();
+			}else{
+				alert("adios");
+				document.getElementById('btn_play').style.display="block";
+				document.getElementsByClassName('categoria_dos_r')[0].innerHTML = "Puntos: "+puntaje2;
+			}
+			break;
+		case('3'):
+			var pregunta1 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var pregunta2 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var pregunta3 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var respuesta1 = "si",
+				respuesta2 = "no",
+				respuesta3 = "si",
+				puntaje3	   = 0 ;
+			if (pregunta1 == respuesta1) {
+	    		puntaje3++;
+			}
+			if(pregunta2 == respuesta2){
+	    		puntaje3++;
+			}
+			if(pregunta3 == respuesta3){
+	    		puntaje3++;
+			}
+			console.log(puntaje3);
+			if(confirm("Desea seguir jugando?")){
+				preguntas();
+			}else{
+				alert("adios");
+				document.getElementById('btn_play').style.display="block";
+				document.getElementsByClassName('categoria_tres_r')[0].innerHTML = "Puntos: "+puntaje3;
+			}
+			break;
+		case('4'):
+			var pregunta1 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var pregunta2 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var pregunta3 = prompt('¿Brendan Eich desarrolló el lenguaje JavaScript?');
+			var respuesta1 = "si",
+				respuesta2 = "no",
+				respuesta3 = "si",
+				puntaje4	   = 0 ;
+			if (pregunta1 == respuesta1) {
+	    		puntaje4++;
+			}
+			if(pregunta2 == respuesta2){
+	    		puntaje4++;
+			}
+			if(pregunta3 == respuesta3){
+	    		puntaje4++;
+			}
+			console.log(puntaje4);
+			if(confirm("Desea seguir jugando?")){
+				preguntas();
+			}else{
+				alert("adios");
+				document.getElementById('btn_play').style.display="block";
+				document.getElementsByClassName('categoria_cuatro_r')[0].innerHTML = "Puntos: "+puntaje4;
+			}
+			break;
+		default:
+			alert('Por favor ingresar Categoria válida');
+			preguntas();
+	}
+}	    
